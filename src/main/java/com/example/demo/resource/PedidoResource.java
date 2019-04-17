d42c52ba5198982e7b7d14238f4b1add4182c5ba
Alexsandro Pires <allexsandro.pires@gmail.com>
2019-04-17 13:30:38 -0300

    obtendo lista atualizada

--- src/main/java/com/example/demo/resource/PedidoResource.java
+++ src/main/java/com/example/demo/resource/PedidoResource.java
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Pedido;
 
-@RestController("pedidos")
+@RestController
+@RequestMapping("pedidos")
 public class PedidoResource {
 
 	static List<Pedido> pedidos = new ArrayList<>();
